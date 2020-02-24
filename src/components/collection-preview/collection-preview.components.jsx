94a37d4b5fbe2dcbf2f6cf09375a5816337f16eb
meenachinmay <chinmayanand896@gmail.com>
2020-02-25 03:18:27 +0530

    only styles are remaining on shop-page

--- src/components/collection-preview/collection-preview.components.jsx
+++ src/components/collection-preview/collection-preview.components.jsx
@@ -12,7 +12,8 @@ const CollectionPreview = ({ title, items }) => (
         <div className="preview">
 
             {
-                items.map(item => (
+                items.filter((item, idx) => idx < 4)
+                .map(item => (
                     <div key={ item.id }>{item.name}</div>
                 ))
             }
